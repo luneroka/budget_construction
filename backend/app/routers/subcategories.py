0603f8b96e717626e576c9405d9a0321dfa68dcb
Luneroka <luneroka@gmail.com>
2026-05-22 15:58:56 +0200

    fix(subcategories): move the /by-category/{category_id} route above /{subcategory_id} to avoid FastAPI confusion

--- backend/app/routers/subcategories.py
+++ backend/app/routers/subcategories.py
@@ -14,6 +14,20 @@ async def get_subcategories(db: AsyncSession = Depends(get_db_session)):
     return await subcategory_repository.get_subcategories(db)
 
 
+# API ENDPOINT TO GET ALL SUBCATEGORIES FOR A SPECIFIC CATEGORY_ID
+@router.get(
+    '/by-category/{category_id}',
+    response_model=list[SubcategoryRead],
+    status_code=status.HTTP_200_OK,
+)
+async def get_subcategories_by_category_id(
+    category_id: int, db: AsyncSession = Depends(get_db_session)
+):
+    return await subcategory_repository.get_subcategories_by_category_id(
+        db, category_id
+    )
+
+
 # API ENDPOINT TO GET A SUBCATEGORY BY ID
 @router.get(
     '/{subcategory_id}', response_model=SubcategoryRead, status_code=status.HTTP_200_OK
@@ -29,17 +43,3 @@ async def get_subcategory(
         )
 
     return subcategory
-
-
-# API ENDPOINT TO GET ALL SUBCATEGORIES FOR A SPECIFIC CATEGORY_ID
-@router.get(
-    '/by-category/{category_id}',
-    response_model=list[SubcategoryRead],
-    status_code=status.HTTP_200_OK,
-)
-async def get_subcategories_by_category_id(
-    category_id: int, db: AsyncSession = Depends(get_db_session)
-):
-    return await subcategory_repository.get_subcategories_by_category_id(
-        db, category_id
-    )
