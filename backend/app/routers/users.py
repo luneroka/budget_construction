257fbbc485f3da7abb13437936bae3c1f8e72c2b
Luneroka <luneroka@gmail.com>
2026-06-01 14:39:10 +0200

    fix(users): add protection so /users/{user_id} endpoint only fetch user's own record

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -18,7 +18,16 @@ async def get_me(current_user: User = Depends(get_current_user)):
 
 # API ENDPOINT TO GET USER BY ID
 @router.get('/{user_id}', response_model=UserRead)
-async def get_user(user_id: int, db: AsyncSession = Depends(get_db_session)):
+async def get_user(
+    user_id: int,
+    db: AsyncSession = Depends(get_db_session),
+    current_user: User = Depends(get_current_user),
+):
+    if user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='User not found'
+        )
+
     user = await user_repository.get_user_by_id(db, user_id)
 
     if user is None:
